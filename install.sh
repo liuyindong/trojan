8a122d823ce577377c875845f0fa775c3be9d7d0
刘银东 <liuyindong@sxmaps.com>
2020-12-21 22:24:42 +0800

    config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Author: Jrohy
-# github: https://github.com/Jrohy/trojan
+# Author: liuyindong
+# github: https://github.com/liuyindong/trojan
 
 #定义操作变量, 0为否, 1为是
 HELP=0
@@ -9,11 +9,11 @@ REMOVE=0
 
 UPDATE=0
 
-DOWNLAOD_URL="https://github.com/Jrohy/trojan/releases/download/"
+DOWNLAOD_URL="https://github.com/liuyindong/trojan/releases/download/"
 
-VERSION_CHECK="https://api.github.com/repos/Jrohy/trojan/releases/latest"
+VERSION_CHECK="https://api.github.com/repos/liuyindong/trojan/releases/latest"
 
-SERVICE_URL="https://raw.githubusercontent.com/Jrohy/trojan/master/asset/trojan-web.service"
+SERVICE_URL="https://raw.githubusercontent.com/liuyindong/trojan/master/asset/trojan-web.service"
 
 [[ -e /var/lib/trojan-manager ]] && UPDATE=1
 
